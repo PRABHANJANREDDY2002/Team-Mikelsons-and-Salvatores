1e16180a328902641d5d7cd4f6579adacc3f1f14
pjvk <98143258+pjvk@users.noreply.github.com>
2022-03-23 00:29:58 +0530

    Add files via upload

---
+++ week 3/Credit_version1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from BANK MANAGEMENT SYSTEM.Transactions import Transactions
+
+class Credit(Transactions):
+    def __init__(self):
+        self.Credit_id = None
+        self.Credit_Amount = None
+        self.Credit_Name = None
+        self.Credit_Type = None
+        self.Credit_Total = None
+
+    def Deposit(self, ):
+        pass
+
+    def Update_Balance(self, ):
+        pass
+
