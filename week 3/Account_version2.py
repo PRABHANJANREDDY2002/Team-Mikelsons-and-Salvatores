1be693560db0e45b5a9471b294693cce2172627e
Tallapallesaijayanth <53572874+Tallapallesaijayanth@users.noreply.github.com>
2022-03-23 00:33:26 +0530

    Add files via upload

---
+++ week 3/Account_version2.py
@@ -0,0 +1,14 @@
+class Account(customer):
+    
+    def __init__(self,balance,customer_name):
+    self.balance=balance
+    self.customer_name=customer_name
+    
+    def updateAccount(self,name,adharCard,PhoneNum,Dob,Address,Account_num):
+        pass
+    
+    def returnBalance(self):
+        return self.balance
+    
+    def customerDetails(self):
+        pass
